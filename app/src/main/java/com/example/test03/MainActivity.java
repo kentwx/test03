b6dfba00b6d8213db1cc4141b53702fa261a9184
jim871013 <69022250+jim871013@users.noreply.github.com>
2020-07-31 12:38:10 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test03/MainActivity.java
+++ app/src/main/java/com/example/test03/MainActivity.java
@@ -46,9 +46,9 @@ public class MainActivity extends AppCompatActivity {
                     else if (18.5 <= fresult && fresult< 24)
                         dia.setText("正常範圍，請繼續保持");
                     else if (24 <=fresult && fresult < 27)
-                        dia.setText("過重ㄛ!請多加注意");
+                        dia.setText("過重ㄛ!小心點");
                     else if (27 <=fresult && fresult < 30)
-                        dia.setText("輕度肥胖");
+                        dia.setText("輕度肥胖，請注意");
                     else if (30 <= fresult && fresult < 35)
                         dia.setText("中度肥胖");
                     else if (fresult >= 35)
